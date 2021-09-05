e9b44fb6df250920c8af46c59955c5559e526fc3
adebadayo <adebayanen@gmail.com>
2021-09-05 14:57:24 +0900

    training action cable

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,20 @@
+import consumer from "./consumer"
+
+window.App = consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    alert(data['message'])
+  },
+
+  speak: function(message) {
+    return this.perform('speak', {message : message});
+  }
+});
